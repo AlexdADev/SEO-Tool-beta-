5abb064879ff4d0c8203bdc0115418e60a5c1cfb
AlexdA_Dev <122083925+AlexdADev@users.noreply.github.com>
2025-02-02 00:13:01 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PageRankApp from './components/PageRankApp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <PageRankApp/>
-);
